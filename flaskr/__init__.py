00ca816082f254c9ad244bc28f27ea6a7243b54d
BenjaminKovacs <bach1099-btkamazonwebservices@yahoo.com>
2018-11-13 12:16:59 -0500

    sockets work

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO
+from flask_socketio import send, emit
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
@@ -12,6 +13,12 @@ def hello():
 @app.route('/')
 def index():
     return render_template('index.html')
+    
+@socketio.on('my event')
+def handle_message(message):
+    print('received message: ' + str(message))
+    print('going to send a message now')
+    emit('test','sockets work!')
 
 if __name__ == '__main__':
     socketio.run(app)
