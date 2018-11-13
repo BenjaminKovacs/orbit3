0f6f4281efcd565f4d54e07aad0f3f1fb234e899
BenjaminKovacs <bach1099-btkamazonwebservices@yahoo.com>
2018-11-13 17:49:01 -0500

    test drawing circle

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -2,6 +2,35 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO
 from flask_socketio import send, emit
 
+class Circle(object):
+    circleList = []
+    circleSend = []
+    def __init__(this, x,y,r,color):
+        this.x = x
+        this.y = y
+        this.r = r
+        this.color = color
+        Circle.circleList.append(this)
+        Circle.circleSend.append(this.getDict())
+        
+    def __repr__(this):
+        return (this.x,this.y,this.r,this.color)
+        
+    def getDict(this):
+        return {'x':this.x,
+                'y':this.y,
+                'r':this.r,
+                'color':this.color}
+                
+    @staticmethod
+    def getSend():
+        Circle.circleSend = []
+        for circle in Circle.circleList:
+            Circle.circleSend.append(circle.getDict()) 
+        return Circle.circleSend
+                
+# socket.io setup code from https://flask-socketio.readthedocs.io/en/latest/
+# flask setup code from http://flask.pocoo.org/docs/1.0/tutorial/
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
@@ -13,12 +42,15 @@ def hello():
 @app.route('/')
 def index():
     return render_template('index.html')
+
+c = Circle(10,10,50,'red')
     
 @socketio.on('my event')
 def handle_message(message):
     print('received message: ' + str(message))
     print('going to send a message now')
     emit('test','sockets work!')
+    emit('update', Circle.getSend())
 
 if __name__ == '__main__':
     socketio.run(app)
---
+++ flaskr/classes.py
@@ -0,0 +1,11 @@
+class Circle(object):
+    circleList = []
+    def __init__(this, x,y,r,color):
+        this.x = x
+        this.y = y
+        this.r = r
+        this.color = color
+        circleList.append(this)
+        
+    def drawInfo(this):
+       pass 
